f66d881ea393697f91662f4d82e9218ba19c8433
Ryan William Tierney <ryan.tierney92@gmail.com>
2019-11-05 19:09:35 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -37,14 +37,14 @@ monopoly = {:railroads  =>
     :b_and_o_railroad =>
   {},
     :shortline =>
-  {},
+  {}
   :rent_in_dollars => 
   {
     :one_piece_owned => 25,
     :two_pieces_owned => 50,
     :three_pieces_owned => 100,
     :four_pieces_owned => 200
-}}}
+}}}}
 end
 
 def monopoly_with_fourth_tier
